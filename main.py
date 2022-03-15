2fdf9603d286495be74d41ce4b18ca84a5580981
matievisthekat <matievisthekat@gmail.com>
2022-03-15 15:28:11 +0200

    use autopep8

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import discord
+import os
+
 
 class TimeSync(discord.Client):
-  async def on_ready(self):
+    async def on_ready(self):
         print('Logged on as', self.user)
 
 
 client = TimeSync()
-client.run(token)
+client.run(os.environ('TOKEN'))
